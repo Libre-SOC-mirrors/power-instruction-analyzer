54b65b4fcc322e5a47ec0f9a556e87858e86b90a
Jacob Lifshay <programmerjake@gmail.com>
2020-07-08 21:35:09 -0700

    update gen-output to use native_instrs by default

--- gen-output.sh
+++ gen-output.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-exec cargo run > "output-for-`git describe`.json"
+case "$1" in
+    --model-only)
+        args=()
+        ;;
+    -h|--help)
+        echo "usage: $0 [--model-only] [-h|--help]"
+        exit
+        ;;
+    '')
+        args=("--features=native_instrs")
+        ;;
+    *)
+        echo "invalid option; use --help for help" >&2
+        exit 1
+        ;;
+esac
+exec cargo run "${args[@]}" > "output-for-`git describe`.json"
+
